785bad29e82f0734b83ed75544d50a308b7c326d
YYF-10 <2427505278@qq.com>
2026-01-04 16:55:35 +0800

    bianjibu

--- backend/src/main/java/com/bjfu/paperSystem/editor/controller/editorController.java
+++ backend/src/main/java/com/bjfu/paperSystem/editor/controller/editorController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/editor")
-public class EditorController {
+public class editorController {
 
     @GetMapping
     public String index(HttpSession session, Model model) {

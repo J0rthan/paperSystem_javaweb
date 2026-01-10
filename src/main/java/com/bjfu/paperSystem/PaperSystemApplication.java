321342d4c25bcbd15b658e755e9643f8588dc585
J0rthan <Jorthan.Leo@Gmail.com>
2026-01-10 14:40:07 +0800

    change file structure

--- src/main/java/com/bjfu/paperSystem/PaperSystemApplication.java
+++ src/main/java/com/bjfu/paperSystem/PaperSystemApplication.java
@@ -2,11 +2,13 @@ package com.bjfu.paperSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableScheduling
+@ServletComponentScan(basePackages = "com.bjfu.paperSystem")
 public class PaperSystemApplication {
 
 	public static void main(String[] args) {
